ba4ca3e01706bbaef7e601ea2519858f1f9c36e3
Roy Herrera <royherrera576@gmail.com>
2022-03-23 15:02:33 -0600

    Nuevos Cambios

--- index.js
+++ index.js
@@ -1,6 +1,5 @@
-// const fetch = require('node-fetch')
 const API = 'https://pokeapi.co/api/v2/pokemon/'
-const contenedor = document.querySelector('.row')
+const contenedor = document.querySelector('.segundaFila')
 
 
 async function asincronismo() {
@@ -10,25 +9,50 @@ async function asincronismo() {
     let PokemonesPerfiles = await Promise.all(pokemones.results.map(p => fetch(p.url)))
 
     let PokemonesPerfilesJSON = await Promise.all(PokemonesPerfiles.map(response => response.json()))
+    renderHTML(PokemonesPerfilesJSON)
+}
 
-    console.log(PokemonesPerfilesJSON);
-    PokemonesPerfilesJSON.forEach((p) => {
+asincronismo()
+
+function renderHTML(pokemonesLista) {
+    let contador = 0;
+    let acumulador = 0;
+    let columnas = "";
+    let numero = 0;
+    
+    pokemonesLista.forEach((p) => {
+        contador++
+        acumulador++
+        numero++
+
         let abilidades = []
-
         p.abilities.forEach((x) => {
             abilidades.push(x.ability.name)
         })
 
-        contenedor.innerHTML += `
-            <div class="card col-md-3">
-                <img src="${p.sprites.other.dream_world.front_default}" class="card-img-top" alt="...">
-                <div class="card-body">
-                    <p class="card-text">Nombre: ${p.name}</p>
-                    <p class="card-text">Habilidades: ${abilidades.toString().split(",").join(", ")}</p>
-                    <p class="card-text">Experiencia: ${p.base_experience}</p>
+        columnas += `
+        <div class="col-md-6 col-lg-3">
+            <div class="card border-dark mb-3">
+                <div class="card-body text-success" style="height: 240px;">
+                    <span>${numero}</span>
+                    <img class="card-img-top" src="${p.sprites.other.dream_world.front_default}" alt="">
                 </div>
-            </div>`
+                <div class="card-footer bg-transparent border-dark">
+                    <p class="card-text">Nombre: ${p.name}</p>
+                    <small class="card-text">Habilidades: ${abilidades.toString().split(",").join(", ")}</small>
+                    <small class="card-text">Experiencia: ${p.base_experience}</small>
+                </div>
+            </div>
+        </div>`
     })
-}
 
-asincronismo()
+    if(contador == 3 || acumulador == pokemonesLista.length) {
+        var fila = document.createElement('div') 
+        fila.classList.add('row')
+        fila.innerHTML = columnas
+        contenedor.appendChild(fila)
+        contador = 0
+        columnas = ""
+        numero = 0
+    }
+}
